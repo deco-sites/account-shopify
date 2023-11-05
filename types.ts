8aa5d321e6b18e9113d2d93a4ce25302e2cddd1f
HUGO GALVAO <hugoaddobbatigalvao@hotmail.com>
2023-11-05 01:44:00 -0300

    create Address Type

--- types.ts
+++ types.ts
@@ -19,12 +19,32 @@ export interface UserOrder {
 }
 
 export interface CustomerInfo {
-  id: string
-  firstName: string
-  lastName: string
-  acceptsMarketing: boolean
-  email: string
-  phone: string
+  id: string;
+  firstName: string;
+  lastName: string;
+  acceptsMarketing: boolean;
+  email: string;
+  phone: string;
+}
+
+export interface Address {
+  id: string;
+  customer_id: string;
+  first_name: string;
+  last_name: string;
+  company: string;
+  address1: string;
+  address2: string;
+  city: string;
+  province: string;
+  country: string;
+  zip: string;
+  phone: string;
+  name: string;
+  province_code: string;
+  country_code: string;
+  country_name: string;
+  default: boolean;
 }
 
 export type UserOrders = UserOrder[];
