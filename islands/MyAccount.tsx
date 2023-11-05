904c9731b4f36cdaae1663f8797768cf9386fa5b
HUGO GALVAO <hugoaddobbatigalvao@hotmail.com>
2023-11-05 01:45:15 -0300

    create address view page

--- islands/MyAccount.tsx
+++ islands/MyAccount.tsx
@@ -1,20 +1,23 @@
 import Avatar from "deco-sites/account-shopify/components/ui/Avatar.tsx";
-import { CustomerInfo, UserOrders } from "$store/types.ts";
+import { CustomerInfo, UserOrders, Address } from "$store/types.ts";
 import { useMemo, useState } from "preact/hooks";
 
 export interface Props {
   orders: UserOrders | null;
   userInfo: CustomerInfo | null;
-  productImages: Record<string, string>
+  productImages: Record<string, string>;
+  addresses: Address[];
 }
 
-function Button(
-  { label, onClick, isSelected = false }: {
-    label: string;
-    onClick: (value: string) => void;
-    isSelected?: boolean;
-  },
-) {
+function Button({
+  label,
+  onClick,
+  isSelected = false,
+}: {
+  label: string;
+  onClick: (value: string) => void;
+  isSelected?: boolean;
+}) {
   return (
     <button
       onClick={() => onClick(label)}
@@ -36,27 +39,31 @@ function Field({ label, value }: { label: string; value: string }) {
   );
 }
 
-function MyAccount({ orders, userInfo, productImages }: Props) {
+function MyAccount({ orders, userInfo, productImages, addresses }: Props) {
   const [selectedOption, setSelectedOption] = useState("Dados");
-  console.log('productImages', productImages)
+  const [selectedAddressIndex, setSelectedAddressIndex] = useState(0);
+  console.log("addresses", addresses);
+  console.log("userInfo", userInfo);
+  console.log("productImages", productImages);
+
+  const selectedAddress =
+    addresses.length > 0 ? addresses[selectedAddressIndex] : undefined;
 
   const initials = useMemo(
     () =>
       userInfo && userInfo.firstName && userInfo.lastName
         ? userInfo.firstName.charAt(0) + userInfo.lastName.charAt(0)
         : "",
-    [userInfo],
+    [userInfo]
   );
 
-  console.log('orders', orders)
+  console.log("orders", orders);
   return (
     <div class="px-44 py-10 bg-gray-100">
       <div class="flex gap-10">
         <div class="flex flex-col justify-between shadow-md bg-white rounded-md">
           <div class="p-10 text-xl flex gap-5 flex-col h-full items-center justify-center">
-            <Avatar
-              content={initials}
-            />
+            <Avatar content={initials} />
             <div class="flex flex-row">
               <div class="mr-1">Olá</div>
               <b>{userInfo?.firstName}</b>
@@ -73,63 +80,132 @@ function MyAccount({ orders, userInfo, productImages }: Props) {
               label="Pedidos"
               isSelected={selectedOption === "Pedidos"}
             />
-            <Button onClick={() => {}} label="Endereços" />
+            <Button
+              onClick={setSelectedOption}
+              label="Endereços"
+              isSelected={selectedOption === "Endereços"}
+            />
             <Button onClick={() => {}} label="Cartões" />
             <Button onClick={() => {}} label="Sair" />
           </div>
         </div>
-        {selectedOption === "Dados"
-          ? (
-            <div class="rounded-md w-3/5">
-              <div class="text-3xl text-gray-700 font-bold mb-6">
-                Dados Pessoais
+        {selectedOption === "Dados" && (
+          <div class="rounded-md w-3/5">
+            <div class="text-3xl text-gray-700 font-bold mb-6">
+              Dados Pessoais
+            </div>
+            <div class="shadow-md bg-white w-full">
+              <div class="grid grid-cols-2 p-10 w-full">
+                <Field label="Nome" value={userInfo?.firstName || ""} />
+                <Field label="Sobrenome" value={userInfo?.lastName || ""} />
               </div>
-              <div class="shadow-md bg-white w-full">
-                <div class="grid grid-cols-2 p-10 w-full">
-                  <Field label="Nome" value={userInfo?.firstName || ""} />
-                  <Field label="Sobrenome" value={userInfo?.lastName || ""} />
-                </div>
-                <div class="grid grid-cols-2 p-10 w-full">
-                  <Field label="E-mail" value={userInfo?.email || ""} />
-                </div>
-                <div class="grid grid-cols-2 p-10 w-full">
-                  <Field label="CPF" value="112.321.654-32" />
-                  <Field label="Gênero" value="Masculino" />
-                </div>
-                <div class="grid grid-cols-2 p-10 w-full">
-                  <Field label="Data de nascimento" value="25/12/1923" />
-                  <Field label="Telefone" value="(83) 9 9664-4966" />
-                </div>
+              <div class="grid grid-cols-2 p-10 w-full">
+                <Field label="E-mail" value={userInfo?.email || ""} />
+              </div>
+              <div class="grid grid-cols-2 p-10 w-full">
+                <Field label="CPF" value="112.321.654-32" />
+                <Field label="Gênero" value="Masculino" />
+              </div>
+              <div class="grid grid-cols-2 p-10 w-full">
+                <Field label="Data de nascimento" value="25/12/1923" />
+                <Field label="Telefone" value="(83) 9 9664-4966" />
               </div>
             </div>
-          )
-          : (
-            <div class="w-3/5">
-              <div class="text-3xl text-gray-700 font-bold mb-6">Pedidos</div>
-              <div class="rounded-md  shadow-md bg-white w-full">
-                <table class="w-full">
-                  <tr class="w-full h-14 border-b">
-                    <th>ID</th>
-                    <th>Preço Total</th>
-                    <th>Data da compra</th>
-                    <th>Status de Pagamento</th>
-                    <th>Imagem</th>
-                  </tr>
-                  {orders?.map((order) => {
-                    return (
-                      <tr class="w-full text-center h-20">
-                        <td>{order.name}</td>
-                        <td>{order.totalPrice}</td>
-                        <td>{(new Date(order.createdAt)).toDateString()}</td>
-                        <td>{order.status}</td>
-                        <td><img src={productImages[order.products[0]]} alt="product_img" class='w-16 m-auto'/></td>
-                      </tr>
-                    );
-                  })}
-                </table>
+          </div>
+        )}
+        {selectedOption === "Pedidos" && (
+          <div class="w-3/5">
+            <div class="text-3xl text-gray-700 font-bold mb-6">Pedidos</div>
+            <div class="rounded-md  shadow-md bg-white w-full">
+              <table class="w-full">
+                <tr class="w-full h-14 border-b">
+                  <th>ID</th>
+                  <th>Preço Total</th>
+                  <th>Data da compra</th>
+                  <th>Status de Pagamento</th>
+                  <th>Imagem</th>
+                </tr>
+                {orders?.map((order) => {
+                  return (
+                    <tr class="w-full text-center h-20">
+                      <td>{order.name}</td>
+                      <td>{order.totalPrice}</td>
+                      <td>{new Date(order.createdAt).toDateString()}</td>
+                      <td>{order.status}</td>
+                      <td>
+                        <img
+                          src={productImages[order.products[0]]}
+                          alt="product_img"
+                          class="w-16 m-auto"
+                        />
+                      </td>
+                    </tr>
+                  );
+                })}
+              </table>
+            </div>
+          </div>
+        )}
+        {selectedOption === "Endereços" && (
+          <div class="rounded-md w-3/5">
+            <div class="text-3xl text-gray-700 font-bold mb-6">
+              Dados do Endereço Selecionado:
+            </div>
+            <div class="w-full flex flex-row justify-start mb-6">
+              <div class="flex flex-col justify-center items-center">
+                <select
+                  onChange={(e) => {
+                    const target = e.target as HTMLSelectElement;
+                    setSelectedAddressIndex(Number(target.value));
+                  }}
+                  class="select select-bordered select-sm h-10"
+                >
+                  {addresses?.map((address, i) => (
+                    <option value={i}>{address?.id}</option>
+                  ))}
+                </select>
               </div>
             </div>
-          )}
+            <div class="shadow-md bg-white w-full">
+              <div class="grid grid-cols-2 p-10 w-full">
+                <Field
+                  label="Nome"
+                  value={selectedAddress?.first_name || "--"}
+                />
+                <Field
+                  label="Sobrenome"
+                  value={selectedAddress?.last_name || "--"}
+                />
+              </div>
+              <div class="grid grid-cols-2 p-10 w-full">
+                <Field
+                  label="Endereço Linha 1"
+                  value={selectedAddress?.address1 || "--"}
+                />
+              </div>
+              <div class="grid grid-cols-2 p-10 w-full">
+                <Field
+                  label="Endereço Linha 2"
+                  value={selectedAddress?.address2 || "--"}
+                />
+              </div>
+              <div class="grid grid-cols-2 p-10 w-full">
+                <Field label="Cidade" value={selectedAddress?.city || "--"} />
+                <Field
+                  label="Estado"
+                  value={selectedAddress?.province || "--"}
+                />
+              </div>
+              <div class="grid grid-cols-2 p-10 w-full">
+                <Field label="CEP" value={selectedAddress?.zip || "--"} />
+                <Field
+                  label="Telefone"
+                  value={selectedAddress?.phone || "--"}
+                />
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
