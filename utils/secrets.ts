9223e0934b77ce96528f50098501ed3a0246e851
ramon38 <ramon@fetch.ly>
2023-11-05 14:54:14 -0300

    Add secrets

--- utils/secrets.ts
+++ utils/secrets.ts
@@ -1,5 +1,8 @@
-export const SHOPIFY_STOREFRONT_ACCESS_TOKEN = Deno.env.get(
-  "SHOPIFY_STOREFRONT_ACCESS_TOKEN",
-) ?? "";
-export const SHOPIFY_ACCESS_TOKEN = Deno.env.get("SHOPIFY_ACCESS_TOKEN") ?? "";
-export const SHOPIFY_STORE_NAME = Deno.env.get("SHOPIFY_STORE_NAME") ?? "";
+// export const SHOPIFY_STOREFRONT_ACCESS_TOKEN = Deno.env.get(
+//   "SHOPIFY_STOREFRONT_ACCESS_TOKEN",
+// ) ?? "";
+// export const SHOPIFY_ACCESS_TOKEN = Deno.env.get("SHOPIFY_ACCESS_TOKEN") ?? "";
+// export const SHOPIFY_STORE_NAME = Deno.env.get("SHOPIFY_STORE_NAME") ?? "";
+export const SHOPIFY_STOREFRONT_ACCESS_TOKEN = "79af056282d74bb4d717152572a3a7ec";
+export const SHOPIFY_ACCESS_TOKEN = "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";
+export const SHOPIFY_STORE_NAME = "ramonetmal2";
